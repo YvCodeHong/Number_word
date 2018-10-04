ddb5fbf4418515b8e2a9b512bdbb38995723a2c3
Yvonne Tang <ytang604@gmail.com>
2018-10-04 23:26:00 +0100

    first commit

---
+++ spec/number_to_words_spec.rb
@@ -0,0 +1,15 @@
+require('number_word')
+
+
+describe('number_word') do
+  it 'can return "one" when I enter 1' do
+    expect(number_word(1)).to eq "one"
+  end
+
+  it 'can return "twenty" when I enter 20'do
+    expect(number_word(20)).to eq "twenty"
+  end
+
+  it 'can return '
+
+end
